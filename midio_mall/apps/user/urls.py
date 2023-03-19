217328fb0f2447b26d70f3a196fd7debec787457
lichuang <lichuang@lc>
2023-03-19 18:58:39 +0800

    注册功能的实现

--- midio_mall/apps/user/urls.py
+++ midio_mall/apps/user/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from apps.user.views import UsernameCountView
+from apps.user.views import UsernameCountView, RegistereView
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
 
     url(r'^usernames/(?P<username>\w{0,20})/count/$', UsernameCountView.as_view()),
+    url(r'^register/$', RegistereView.as_view()),
 
 ]
--- midio_mall/apps/user/views.py
+++ midio_mall/apps/user/views.py
@@ -1,3 +1,4 @@
+import json
 import re
 
 from django.shortcuts import render
@@ -26,6 +27,7 @@ from django.views import View
 from apps.user.models import User
 from django.http import JsonResponse
 import re
+
 class UsernameCountView(View):
     def get(self, request, username):
         # 1.接收用户名
@@ -35,3 +37,69 @@ class UsernameCountView(View):
             return JsonResponse({'code': 200, 'count': count, "errmsg": 'username  model is not use'})
         # 3.返回响应
         return JsonResponse({'code': 0, 'count': count, "errmsg": 'ok'})
+
+"""
+前端：	用户输入用户名，密码，确认密码，手机号，是否同意协议后，点击注册按钮
+		发送请求
+
+后端：
+请求		接收json数据，获取数据
+业务逻辑		验证数据，数据入库
+响应		JSON{‘code':0,errmsg:'ok'}
+路由		POST	register/
+
+步骤：
+接受请求（POST---JSON）
+对请求进行获取和解码
+获取数据
+验证数据
+数据不为空
+数据合规
+数据入库
+返回响应
+"""
+class RegistereView(View):
+    def post(self, request):
+        import json
+        # 1.接收参数：请求体中的JSON数据 request.body
+        json_bytes = request.body  # 从请求体中获取原始的JSON数据，bytes类型的
+        json_str = json_bytes.decode()  # 将bytes类型的JSON数据，转成JSON字符串
+        body_dict = json.loads(json_str)  # 将JSON字符串，转成python的标准字典
+        # json_dict = json.loads(request.body.decode())
+
+
+
+        # 获取数据
+        username = body_dict.get('username')
+        password = body_dict.get('password')
+        password2 = body_dict.get('password2')
+        mobile = body_dict.get('mobile')
+        allow = body_dict.get('allow')
+
+        # 验证数据
+        if not all([username,password,password2,mobile,allow]):
+            return JsonResponse({'code': 400, 'errmsg': "参数不全"})
+        # 判断用户名是否是5-20个字符
+        if not re.match(r'^[a-zA-Z0-9_]{5,20}$', username):
+            return JsonResponse({'code': 400, 'errmsg': "username格式有误!"})
+        # 判断密码是否是8-20个数字
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return JsonResponse({'code': 400, 'errmsg': "password格式有误!"})
+        # 判断两次密码是否一致
+        if password != password2:
+            return JsonResponse({'code': 400, 'errmsg': "两次输入不对!"})
+        # 判断手机号是否合法
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return JsonResponse({'code': 400, 'errmsg':" 'mobile格式有误!'"})
+        # 判断是否勾选用户协议
+        if allow != True:
+            return JsonResponse({'code': 400, 'errmsg': "allow格式有误!"})
+
+        # 数据入库
+        User.objects.create_user(
+            username=username,
+            password=password,
+            mobile=mobile,
+        )
+        # 返回响应
+        return JsonResponse({'code': 0, 'errmsg': "ok!"})
