6ced9c0aa7241e93d11f16a893d554e7d08f65de
lichuang <lichuang@lc>
2023-06-05 16:52:22 +0800

    统计分类商品访问量实现

--- midio_mall/apps/goods/urls.py
+++ midio_mall/apps/goods/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from apps.goods.views import IndexView, ListView, SearchView,DetailView
+from apps.goods.views import IndexView, ListView, SearchView,DetailView,CategoryVisitCountView
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^list/(?P<category_id>\d+)/skus/', ListView.as_view()),
     url(r'^search/', SearchView()),
     url(r'^detail/(?P<sku_id>\d+)/', DetailView.as_view()),
+    url(r'detail/visit/(?P<category_id>\d+)/', CategoryVisitCountView.as_view()),
 
 ]
--- midio_mall/apps/goods/views.py
+++ midio_mall/apps/goods/views.py
@@ -192,3 +192,54 @@ class DetailView(View):
             'specs': good_specs,
         }
         return render(request,'detail.html',content)
+
+
+"""
+需求：
+    统计每一天的分类商品访问量
+前端：
+    当访问具体的页面的时候，会发送一个axios请求，携带分类id
+后端：
+    请求：接收请求，获取参数
+    业务逻辑：查询有没有，有的话更新数据，没有的话新建数据
+    响应：返回JSON响应
+
+    路由：POST 新增数据  detail/visit/<category_id>/
+    步骤：
+        1.接收分类id
+        2.验证参数（验证分类id）
+        3.查询当天 这个分类的记录有没有
+        4.没有 新建数据
+        5.有的话更新数据
+        6.返回响应
+
+"""
+from apps.goods.models import GoodsVisitCount
+from _datetime import date
+
+
+class CategoryVisitCountView(View):
+    """一天之内，某个三级分类 商品的访问量"""
+
+    def post(self, request, category_id):
+        # 1.接收分类id
+        # 2.验证参数
+        try:
+            category = GoodsCategory.objects.get(id=category_id)
+        except GoodsCategory.DoesNotExist:
+            return JsonResponse({'code': 400, 'errmsg': '没有此分类'})
+        # 3.查询当天 这个分类的记录有没有 显示今天的日期
+        today = date.today()
+        try:
+            gvc = GoodsVisitCount.objects.get(category=category, date=today)
+        except GoodsVisitCount.DoesNotExist:
+            # 4.没有创建数据
+            GoodsVisitCount.objects.create(category=category,
+                                           date=today,
+                                           count=1)
+        else:
+            # 5.有的话更新数据
+            gvc.count += 1
+            gvc.save()
+        # 6.返回响应
+        return JsonResponse({'code': 0, 'errmsg': 'ok'})
